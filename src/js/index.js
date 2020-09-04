784e7a724a8efe011cae29519bebda7889c1795f
StephSince93 <stephen@stemsoftware.com>
2020-09-04 16:02:56 -0500

    Added complex pagination

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@
 // console.log(`Using imported function ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(searchView.ID, 4)}. ${string}`)
 import Search from './models/Search'
 import * as searchView from './views/searchView'
-import { elements } from './views/base'
+import { elements, renderLoader, clearLoader } from './views/base'
 /** Global state of the app 
  *  - Search object
  *  - Current recipe object
@@ -26,10 +26,12 @@ const controlSearch = async () => {
         // 3) Prepare UI for results
         searchView.clearInput()
         searchView.clearResults()
+        renderLoader(elements.searchRes)
         // 4) Search for recipes
         await state.search.getResults()
 
         // 5) Render results on UI
+        clearLoader()
         searchView.renderResults(state.search.result)
     }
 }
@@ -37,4 +39,15 @@ const controlSearch = async () => {
 elements.searchForm.addEventListener('submit', e => {
     e.preventDefault()
     controlSearch()
+})
+
+elements.searchRes.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline')
+    
+    if(btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10)
+        
+        searchView.clearResults()
+        searchView.renderResults(state.search.result, goToPage)
+    }
 })
--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -3,7 +3,10 @@ import { elements } from './base'
 export const getInput = () => elements.searchInput.value
 
 export const clearInput = () => { elements.searchInput.value = '' }
-export const clearResults = () => { elements.searchResultList.innerHTML = '' }
+export const clearResults = () => { 
+    elements.searchResultList.innerHTML = '',
+    elements.searchResPages.innerHTML = ''
+    }
 /**
  *  'Pasta with tomato and spinach'
  * acc: 0 / acc + cur.length = 5 / newTitle = ['Pasta']
@@ -45,7 +48,43 @@ const renderRecipe = recipe => {
     `
     elements.searchResultList.insertAdjacentHTML('beforeend', markup)
 }
+//type: 'prev' or 'next'
+const createButton = (page, type) => `
+            <button class="btn-inline results__btn--${type}" data-goto="${type === 'prev' ? page - 1 : page + 1}">
+            <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+                <svg class="search__icon">
+                    <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+                </svg>
+            </button>
+            `
 
-export const renderResults = recipes => {
-    recipes.forEach(renderRecipe);
+const renderButtons = (page, numResults, resPerPage) => {
+    const pages = Math.ceil(numResults / resPerPage)
+    let button
+    if (page === 1) {
+        // Only button to go to next page
+        button = createButton(page, 'next')
+    }else if(page < pages){
+        // Both Buttons
+        button = `
+            ${createButton(page, 'prev')}
+            ${createButton(page, 'next')}
+            `
+    }else if(page === pages && pages > 1) {
+        // Only button to go to prev page
+        button = createButton(page, 'prev')
+    }
+
+    elements.searchResPages.insertAdjacentHTML('afterbegin', button)
+}
+
+export const renderResults = (recipes, page = 1, resPerPage=10) => {
+    //render results of current page
+
+    const start = (page -1) * resPerPage
+    const end = page * resPerPage
+    recipes.slice(start,end).forEach(renderRecipe);
+
+    //render pagination buttons
+    renderButtons(page, recipes.length, resPerPage)
 }
