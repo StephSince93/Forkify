4bd7447159a4dc966fe6ece39ebb9d5815ee2f99
StephSince93 <stephen@stemsoftware.com>
2020-09-04 11:33:32 -0500

    Added api calls with axios plugin

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,18 @@
-import string from './models/Search'
-//import { add as a, multiply as m, ID } from './views/searchView'
-import * as searchView from './views/searchView' 
+// import string from './models/Search'
+// import { add as a, multiply as m, ID } from './views/searchView'
+// import * as searchView from './views/searchView' 
 
-console.log(`Using imported function ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(searchView.ID, 4)}. ${string}`)
+// console.log(`Using imported function ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(searchView.ID, 4)}. ${string}`)
+import axios from 'axios'
+
+async function getResults(query) {
+    try {
+        const {data} = await axios.get(`https://forkify-api.herokuapp.com/api/search?q=${query}`)
+        console.log(data.recipes)
+    }catch(e) {
+        alert(e)
+    }
+
+}
+
+getResults('tomato')
