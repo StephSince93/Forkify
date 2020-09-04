a4cbcdefd0aaf6f459b730a1b02a2eeb12fd789e
StephSince93 <stephen@stemsoftware.com>
2020-09-04 13:01:07 -0500

    Search button and api call new rendering data and disaplying properly

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,37 @@
 
 // console.log(`Using imported function ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(searchView.ID, 4)}. ${string}`)
 import Search from './models/Search'
+import * as searchView from './views/searchView'
+import { elements } from './views/base'
+/** Global state of the app 
+ *  - Search object
+ *  - Current recipe object
+ *  - Shopping list object
+ *  - Liked recipes
+*/
+const state = {}
 
-const search = new Search('pizza')
+const controlSearch = async () => {
+    // 1) get query from view
+    const query = searchView.getInput() //TODO
+    console.log(query)
 
-search.getResults()
+    if (query) {
+        // 2) New search object and add it to state
+        state.search = new Search(query)
 
-console.log(search)
+        // 3) Prepare UI for results
+        searchView.clearInput()
+        searchView.clearResults()
+        // 4) Search for recipes
+        await state.search.getResults()
+
+        // 5) Render results on UI
+        searchView.renderResults(state.search.result)
+    }
+}
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault()
+    controlSearch()
+})
